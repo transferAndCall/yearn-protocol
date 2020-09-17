d84283bfc04be5d77e0f2eeacde266329432500f
transferAndCall <transferAndCall@protonmail.com>
2020-09-16 19:07:42 -0500

    Add governance access control tests

--- test/yDelegatedvault.js
+++ test/yDelegatedvault.js
@@ -2,6 +2,7 @@ const Aave = artifacts.require('MockAave')
 const Controller = artifacts.require('Controller')
 const Token = artifacts.require('Token')
 const yDelegatedVault = artifacts.require('yDelegatedVault')
+const { expectEvent, expectRevert } = require('@openzeppelin/test-helpers')
 
 contract('yDelegatedVault', (accounts) => {
     const deployer = accounts[0]
@@ -24,10 +25,41 @@ contract('yDelegatedVault', (accounts) => {
             aave.address,
             { from: deployer }
         )
+        await vault.setGovernance(
+            governance,
+            { from: deployer }
+        )
     })
 
     it('has expected state on deployment', async () => {
         assert.equal(aave.address, await vault.aave())
         assert.equal(token.address, await vault.token())
+    })
+
+    it('only allows governance to call setHealthFactor', async () => {
+        await expectRevert(
+            vault.setHealthFactor(1, { from: deployer }),
+            '!governance'
+        )
+        await vault.setHealthFactor(2, { from: governance })
+        assert.equal(2, await vault.healthFactor())
+    })
+
+    it('only allows governance to call setGovernance', async () => {
+        await expectRevert(
+            vault.setGovernance(deployer, { from: deployer }),
+            '!governance'
+        )
+        await vault.setGovernance(user, { from: governance })
+        assert.equal(user, await vault.governance())
+    })
+
+    it('only allows governance to call setController', async () => {
+        await expectRevert(
+            vault.setController(deployer, { from: deployer }),
+            '!governance'
+        )
+        await vault.setController(user, { from: governance })
+        assert.equal(user, await vault.controller())
     })
 })
