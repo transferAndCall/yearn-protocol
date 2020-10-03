d6f6faef4ff32cd4cf39aa7fac26bb1e3582d773
transferAndCall <transferAndCall@protonmail.com>
2020-10-03 15:39:29 +0000

    Convert contracts to use this

--- test/yDelegatedvault.js
+++ test/yDelegatedvault.js
@@ -34,134 +34,132 @@ contract('yDelegatedVault', (accounts) => {
     let onesplit_returnAmount = 1
     let onesplit_distribution = [1]
 
-    let aave, controller, feedReserve, feedStable, lendingPool, pool, oracle, onesplit, strategy, stable, token, vault, underlying
-
     beforeEach(async () => {
-        underlying = await Token.new({ from: deployer })
-        token = await MockAaveToken.new(underlying.address, { from: deployer })
-        stable = await Token.new({ from: deployer })
-        onesplit = await OneSplit.new(
+        this.underlying = await Token.new({ from: deployer })
+        this.token = await MockAaveToken.new(this.underlying.address, { from: deployer })
+        this.stable = await Token.new({ from: deployer })
+        this.onesplit = await OneSplit.new(
             onesplit_returnAmount,
             onesplit_distribution,
             { from: deployer }
         )
         MockV2Aggregator.setProvider(web3.currentProvider)
-        feedReserve = await MockV2Aggregator.new(initialReservePrice, { from: deployer })
-        feedStable = await MockV2Aggregator.new(initialStablePrice, { from: deployer })
-        lendingPool = await LendingPool.new(
-            feedReserve.address,
-            token.address,
-            feedStable.address,
-            stable.address,
+        this.feedReserve = await MockV2Aggregator.new(initialReservePrice, { from: deployer })
+        this.feedStable = await MockV2Aggregator.new(initialStablePrice, { from: deployer })
+        this.lendingPool = await LendingPool.new(
+            this.feedReserve.address,
+            this.token.address,
+            this.feedStable.address,
+            this.stable.address,
             { from: deployer }
         )
-        oracle = await Oracle.new({ from: deployer })
-        aave = await Aave.new(
-            lendingPool.address,
-            oracle.address,
+        this.oracle = await Oracle.new({ from: deployer })
+        this.aave = await Aave.new(
+            this.lendingPool.address,
+            this.oracle.address,
             { from: deployer }
         )
-        pool = await Pool.new(stable.address, { from: deployer })
-        controller = await Controller.new(
+        this.pool = await Pool.new(this.stable.address, { from: deployer })
+        this.controller = await Controller.new(
             rewards,
-            onesplit.address,
+            this.onesplit.address,
             { from: deployer }
         )
-        vault = await yDelegatedVault.new(
-            token.address,
-            controller.address,
-            aave.address,
+        this.vault = await yDelegatedVault.new(
+            this.token.address,
+            this.controller.address,
+            this.aave.address,
             healthFactor,
             { from: deployer }
         )
-        strategy = await Strategy.new(
-            controller.address,
-            onesplit.address,
-            stable.address,
-            pool.address,
+        this.strategy = await Strategy.new(
+            this.controller.address,
+            this.onesplit.address,
+            this.stable.address,
+            this.pool.address,
             { from: deployer }
         )
-        await oracle.setPriceOracle(
-            underlying.address,
-            feedReserve.address,
+        await this.oracle.setPriceOracle(
+            this.underlying.address,
+            this.feedReserve.address,
             { from: deployer }
         )
-        await oracle.setPriceOracle(
-            token.address,
-            feedReserve.address,
+        await this.oracle.setPriceOracle(
+            this.token.address,
+            this.feedReserve.address,
             { from: deployer }
         )
-        await oracle.setPriceOracle(
-            stable.address,
-            feedStable.address,
+        await this.oracle.setPriceOracle(
+            this.stable.address,
+            this.feedStable.address,
             { from: deployer }
         )
-        await controller.setStrategy(
-            vault.address,
-            strategy.address,
+        await this.controller.setStrategy(
+            this.vault.address,
+            this.strategy.address,
             { from: deployer }
         )
-        await vault.setGovernance(
+        await this.vault.setGovernance(
             governance,
             { from: deployer }
         )
-        await controller.setGovernance(
+        await this.controller.setGovernance(
             governance,
             { from: deployer }
         )
-        await strategy.setGovernance(
+        await this.strategy.setGovernance(
             governance,
             { from: deployer }
         )
-        await token.transfer(user1, tokens(1), { from: deployer })
-        await token.transfer(user2, tokens(1), { from: deployer })
-        await stable.transfer(lendingPool.address, tokens(30000), { from: deployer })
+        await this.token.transfer(user1, tokens(1), { from: deployer })
+        await this.token.transfer(user2, tokens(1), { from: deployer })
+        await this.stable.transfer(this.lendingPool.address, tokens(30000), { from: deployer })
     })
 
     it('has expected state on deployment', async () => {
-        assert.equal(aave.address, await vault.aave())
-        assert.equal(token.address, await vault.token())
-        assert.equal(controller.address, await vault.controller())
-        assert.equal('yflink Chainlink aToken', await vault.name())
-        assert.equal('yflaLINK', await vault.symbol())
-        assert.equal(4, await vault.healthFactor())
+        assert.equal(this.aave.address, await this.vault.aave())
+        assert.equal(this.token.address, await this.vault.token())
+        assert.equal(this.controller.address, await this.vault.controller())
+        assert.equal('yflink Chainlink aToken', await this.vault.name())
+        assert.equal('yflaLINK', await this.vault.symbol())
+        assert.equal(4, await this.vault.healthFactor())
     })
 
     describe('deposit', () => {
         it('accepts deposits and provides y token', async () => {
-            await token.increaseAllowance(vault.address, tokens(1), { from: user1 })
-            await vault.deposit(tokens(1), { from: user1 })
-            assert.isTrue(applyFee(tokens(1)).eq(await vault.balanceOf(user1)))
-            assert.isTrue(fee(tokens(1)).eq(await vault.insurance()))
-            assert.isTrue(applyFee(tokens(1)).eq(await vault.balance()))
-            assert.isTrue(applyFee(tokens(1)).eq(await vault.totalSupply()))
+            await this.token.increaseAllowance(this.vault.address, tokens(1), { from: user1 })
+            await this.vault.deposit(tokens(1), { from: user1 })
+            assert.isTrue(applyFee(tokens(1)).eq(await this.vault.balanceOf(user1)))
+            assert.isTrue(fee(tokens(1)).eq(await this.vault.insurance()))
+            assert.isTrue(applyFee(tokens(1)).eq(await this.vault.balance()))
+            assert.isTrue(applyFee(tokens(1)).eq(await this.vault.totalSupply()))
         })
 
         context('after initial deposit', () => {
             beforeEach(async () => {
-                await token.increaseAllowance(vault.address, tokens(1), { from: user1 })
-                await vault.deposit(tokens(1), { from: user1 })
-                assert.isTrue(applyFee(tokens(1)).eq(await vault.balanceOf(user1)))
+                await this.token.increaseAllowance(this.vault.address, tokens(1), { from: user1 })
+                await this.vault.deposit(tokens(1), { from: user1 })
+                assert.isTrue(applyFee(tokens(1)).eq(await this.vault.balanceOf(user1)))
             })
 
             it('accepts additional deposit', async () => {
-                await token.increaseAllowance(vault.address, tokens(1), { from: user2 })
-                await vault.deposit(tokens(1), { from: user2 })
-                assert.isTrue(applyFee(tokens(1)).eq(await vault.balanceOf(user2)))
-                assert.isTrue(fee(tokens(2)).eq(await vault.insurance()))
-                assert.isTrue(applyFee(tokens(2)).eq(await vault.balance()))
-                assert.isTrue(applyFee(tokens(2)).eq(await vault.totalSupply()))
+                await this.token.increaseAllowance(this.vault.address, tokens(1), { from: user2 })
+                await this.vault.deposit(tokens(1), { from: user2 })
+                assert.isTrue(applyFee(tokens(1)).eq(await this.vault.balanceOf(user2)))
+                assert.isTrue(fee(tokens(2)).eq(await this.vault.insurance()))
+                assert.isTrue(applyFee(tokens(2)).eq(await this.vault.balance()))
+                assert.isTrue(applyFee(tokens(2)).eq(await this.vault.totalSupply()))
             })
 
             it('has no credit or debt', async () => {
-                assert.isTrue(new BN(0).eq(await vault.credit()))
-                assert.isTrue(new BN(0).eq(await vault.debt()))
+                assert.isTrue(new BN(0).eq(await this.vault.credit()))
+                assert.isTrue(new BN(0).eq(await this.vault.debt()))
             })
 
             it('allows withdraw', async () => {
-                await vault.withdrawAll({ from: user1 })
-                assert.isTrue(applyFee(tokens(1)).eq(await token.balanceOf(user1)))
-                assert.equal(0, await vault.totalSupply())
+                await this.vault.withdrawAll({ from: user1 })
+                assert.isTrue(applyFee(tokens(1)).eq(await this.token.balanceOf(user1)))
+                assert.equal(0, await this.vault.totalSupply())
             })
         })
     })
@@ -169,33 +167,33 @@ contract('yDelegatedVault', (accounts) => {
     describe('setHealthFactor', () => {
         it('only allows governance to call setHealthFactor', async () => {
             await expectRevert(
-                vault.setHealthFactor(1, { from: deployer }),
+                this.vault.setHealthFactor(1, { from: deployer }),
                 '!governance'
             )
-            await vault.setHealthFactor(2, { from: governance })
-            assert.equal(2, await vault.healthFactor())
+            await this.vault.setHealthFactor(2, { from: governance })
+            assert.equal(2, await this.vault.healthFactor())
         })
     })
 
     describe('setGovernance', () => {
         it('only allows governance to call setGovernance', async () => {
             await expectRevert(
-                vault.setGovernance(deployer, { from: deployer }),
+                this.vault.setGovernance(deployer, { from: deployer }),
                 '!governance'
             )
-            await vault.setGovernance(user1, { from: governance })
-            assert.equal(user1, await vault.governance())
+            await this.vault.setGovernance(user1, { from: governance })
+            assert.equal(user1, await this.vault.governance())
         })
     })
 
     describe('setController', () => {
         it('only allows governance to call setController', async () => {
             await expectRevert(
-                vault.setController(deployer, { from: deployer }),
+                this.vault.setController(deployer, { from: deployer }),
                 '!governance'
             )
-            await vault.setController(user1, { from: governance })
-            assert.equal(user1, await vault.controller())
+            await this.vault.setController(user1, { from: governance })
+            assert.equal(user1, await this.vault.controller())
         })
     })
 })
